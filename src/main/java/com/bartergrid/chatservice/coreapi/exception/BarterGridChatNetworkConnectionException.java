6ac2046a4bccdc86227bc5ad058c9aa3c475df99
NashidForbes <NashidForbes@biohumans.com>
2026-08-02 14:45:16 -0400

    PBI-168 Add custom exception for chat network connection errors

---
+++ src/main/java/com/bartergrid/chatservice/coreapi/exception/BarterGridChatNetworkConnectionException.java
@@ -0,0 +1,7 @@
+package com.bartergrid.chatservice.coreapi.exception;
+
+public class BarterGridChatNetworkConnectionException extends RuntimeException {
+    public BarterGridChatNetworkConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
